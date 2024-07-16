ee1649461449efc08070d4abc687d2a7cdd4047a
reddymanyam <reddappareddy18@gmail.com>
2024-07-16 16:36:35 +0530

    toggle password

--- src/components/Togglepassword.js
+++ src/components/Togglepassword.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 
 const Togglepassword = () => {
-    const[visible, setVisible] = useState(false);
-    const handleVisible = () =>{
-         setVisible(!visible)
-    }
 
+   const[email,setEmail] = useState("");
+   const [visible, setVisible] = useState(false);
+
+   const handleVisible = () =>{
+    setVisible(!visible);
+   }
+
+ 
   return (
   <>
     <h1>Togglepassword</h1>
-    Password:<input type={visible?"text":"password"}/>
+    Email:<input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+    Password:<input type={visible?"text":"password"} />
     <button onClick={handleVisible}>{visible?"👁️":"😵"}</button>
-  </>
+      </>
   )
 }
 

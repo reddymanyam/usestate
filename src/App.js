60540613860c2c38b60f36c441e06540314ad876
reddymanyam <reddappareddy18@gmail.com>
2024-07-16 16:38:45 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Login from "./components/Login";
 import Counter from './components/Counter';
 import Togglepassword from './components/Togglepassword';
+import Arraylist from './components/Arraylist';
+import ToggleTheme from './components/ToggleTheme';
 function App() {
   return (
     <div className="container">
-     <Counter />
      <Login />
+     <Counter />
      <Togglepassword />
+     <Arraylist /> 
+     <ToggleTheme /> 
     </div>
   );
 }
